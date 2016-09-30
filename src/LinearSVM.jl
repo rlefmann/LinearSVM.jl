b6ee6fa08c1fcf35afa9b63f881864983c3eaaa1
Raoul Lefmann <raoul.lefmann@gmail.com>
2016-09-30 13:27:31 +0200

    added source files

--- src/LinearSVM.jl
+++ src/LinearSVM.jl
@@ -1,5 +1,21 @@
 module LinearSVM
 
-# package code goes here
+include("DualCD.jl")
+include("Pegasos.jl")
+include("ADMMDriver.jl")
+#include("ADMMWorker.jl")
+include("Utilities.jl")
+
+# DualCD.jl:
+export dualcd
+export pegasos
+
+# ADMMDriver.jl:
+export distributeData
+export admm
+
+# Utilities.jl:
+export loadDataset
+export testResult
 
 end # module
